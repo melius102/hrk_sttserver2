b6aa68787532ad646fcf27cdbc34de27d903b81c
melius18 <melius18@protonmail.com>
2020-02-04 11:18:37 +0900

    modify for heroku

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const https = require('https');
+const http = require('http');
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // for heroku
 
 // https://192.168.0.11:3000/
 
@@ -19,14 +20,20 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-const privateKey = fs.readFileSync('openssl/private.pem');
-const certificate = fs.readFileSync('openssl/public.pem');
-const credentials = {
-    key: privateKey,
-    cert: certificate
-};
-
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(port, function () {
-    console.log("HTTPS server listening on port " + port);
+const httpServer = http.createServer(app);
+httpServer.listen(port, function () {
+    console.log("HTTP server listening on port " + port);
 });
+
+// https sevrver
+// const privateKey = fs.readFileSync('openssl/private.pem');
+// const certificate = fs.readFileSync('openssl/public.pem');
+// const credentials = {
+//     key: privateKey,
+//     cert: certificate
+// };
+
+// const httpsServer = https.createServer(credentials, app);
+// httpsServer.listen(port, function () {
+//     console.log("HTTPS server listening on port " + port);
+// });
